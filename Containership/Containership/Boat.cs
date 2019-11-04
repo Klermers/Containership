ef898cc1cad76f4f234d6cc744ff26a7f278d36d
Klermers <50822149+Klermers@users.noreply.github.com>
2019-11-04 11:44:00 +0100

    Add files via upload

---
+++ Containership/Containership/Boat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Containership
+{
+    class Boat
+    {
+        private string name;
+        private int length;
+        private int width;
+        private int weight;
+        private int seperatorlenght = 1;
+        private int seperatorwidth = 3;
+        private Boat[, ,] boatcapacity;
+        private Container addcontainer;
+
+        public Boat(string name, int length, int width, int weight )
+        {
+            addcontainer = new Container();
+            this.name = name;
+            this.length = length;
+            this.width = width;
+            this.weight = weight;
+        }
+
+        private void BoatCapicityCalculator()
+        {
+            int totalcontainerlenght = addcontainer.Lenght + seperatorlenght; ;
+            int totalcontainerwidth = addcontainer.Width + seperatorwidth; ;
+
+            int amountcontainerslenght = this.length / totalcontainerlenght;
+            int amountcontainerswidth = this.width / totalcontainerwidth;
+
+            boatcapacity = new Boat[totalcontainerwidth,totalcontainerlenght,];
+        }
+
+    }
+}
---
+++ Containership/Containership/Container.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Containership
+{
+    class Container
+    {
+        private int lenght;
+        private int width;
+        private int height;
+        private int weight;
+        private int currentcapacity;
+        private int maxcapacity;
+
+        public int Lenght
+        {
+            get { return lenght; }
+        }
+
+        public int Width
+        {
+            get { return width; }
+        }
+
+        public int Height
+        {
+            get { return height; }
+        }
+
+        public int Weight
+        {
+            get { return weight; }
+        }
+
+    }
+}
